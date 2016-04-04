d3999115e1f300fa3f19c714a4b5e3df84eba8ab
remotezen <remotezen@bell.net>
2016-04-04 12:03:55 -0400

    Redirect after post and show all types in template

--- lists/tests.py
+++ lists/tests.py
@@ -11,6 +11,11 @@ from lists.models import Item
 
 
 class HomePageTest(TestCase):
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
+
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
@@ -23,6 +28,37 @@ class HomePageTest(TestCase):
         self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do lists</title>', response.content)
         self.assertTrue(response.content.strip().endswith(b'</html>'))
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+        response = home_page(request)
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+        response = home_page(request)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text='Item 1')
+        Item.objects.create(text='Item 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('Item 1', response.content.decode())
+        self.assertIn('Item 1', response.content.decode())
+
+
+
+
 
 class ItemModelTest(TestCase):
     def test_saving_retrieving_items(self):
--- lists/views.py
+++ lists/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from lists.models import  Item
 
 #  make sure you install lists in settings INSTALLED_APPS
 
 
 def home_page(request):
-    return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', '')
-    })
+    if request.method == 'POST':
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text)
+        return redirect('/')
+    else:
+        new_item_text = ''
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
 # Create your views here.
