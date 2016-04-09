6e416a4cc67cc595d8dde4d6a701d7d41a7058b0
remotezen <remotezen@bell.net>
2016-04-09 10:00:37 -0400

    Moved unit test into test/test_all.py

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,3 +1,4 @@
+#base.py
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 import sys
@@ -12,8 +13,29 @@ import sys
         python3 manage.py test lists
  """
 class FunctionalTest(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_host = arg.split('=')[1]
+                cls.server_url = 'http://' + cls.server_host
+                cls.against_staging = True
+                return
+        super().setUpClass()
+        cls.against_staging = False
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if not cls.against_staging:
+            super().tearDownClass()
     def setUp(self):
         self.browser = webdriver.Firefox()
 
     def tearDown(self):
         self.browser.close()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
--- functional_tests/test_list_items_validation.py
+++ functional_tests/test_list_items_validation.py
@@ -1,3 +1,4 @@
+#test_items_validation.py
 from .base import FunctionalTest
 
 from unittest import skip
@@ -11,6 +12,20 @@ from unittest import skip
  """
 
 class ItemValidationTest(FunctionalTest):
-    @skip
     def test_cannot_add_empty_list_items(self):
-        pass
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+
+        error = self.browser.find_element_by_css_selector('has-error')
+
+        self.assertEqual(error.text, "You can't have an empty list item")
+
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
+        self.check_for_row_in_list_table('1: Buy milk')
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+        self.check_for_row_in_list_table('1: Buy milk')
+        error = self.browser.find_element_by_css_selector('has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
+        self.check_for_row_in_list_table('1: Buy milk')
+        self.check_for_row_in_list_table('1: Make tea')
