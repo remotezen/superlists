0bfb25fbd87432db7f6f1131c203f3a0fa40c4f1
remotezen <remotezen@bell.net>
2016-04-11 14:22:49 -0400

    refractored hardcoded urls out of template

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from lists.models import  Item, List
+from lists.models import Item, List
 from django.core.exceptions import ValidationError
 from django.utils.html import escape
 
@@ -8,6 +8,7 @@ from django.utils.html import escape
 
 def home_page(request):
     return render(request, 'home.html')
+
 
 def new_list(request):
     list_ = List.objects.create()
@@ -19,16 +20,24 @@ def new_list(request):
         list_.delete()
         error = escape("You can't have an empty list item")
         return render(request, 'home.html', {"error": error})
-    return redirect('/lists/%d/' %( list_.id,))
+    return redirect('/lists/%d/' % (list_.id,))
 
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
+    error = None
     if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'], list=list_)
-        return redirect('/lists/%d/' % (list_.id,))
-    return render(request, 'list.html', {'list' : list_})
+        try:
+            item = Item(text=request.POST['item_text'], list=list_)
+            item.full_clean()
+            item.save()
+            return redirect('/lists/%d/' % (list_.id,))
+        except ValidationError:
+            error = "You can't have an emtpy list item"
+
+    return render(request, 'list.html', {'list': list_, 'error': error})
 # Create your views here.
+
 
 def add_item(request, list_id):
     list_ = List.objects.get(id=list_id,)
